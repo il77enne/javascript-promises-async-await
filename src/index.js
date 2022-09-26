a3a7945ae1d789f3c37f9d9f13a1abdcbded264d
Stefano Brembati <stefano.brembati@scao.it>
2022-09-26 14:30:20 +0200

    Completed module

--- src/index.js
+++ src/index.js
@@ -83,3 +83,15 @@ getBooksOrMoviesAsync().then(results => {
     });
 });
 
+
+
+
+const timer1 = setTimeout(() => {
+    console.log("timer 1 has finished")
+}, 3000);
+
+
+const timer2 = setTimeout(() => {
+    console.log("timer 2 has finished")
+    clearTimeout(timer1)
+}, 2000);
